f2007386e46a6c7a7c56fcdd2937fbe5f23367f7
Satynaag <93556980+Satynaag@users.noreply.github.com>
2025-07-26 10:06:42 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+// app.use(cors(
+//     {
+//         origin: ["https://quick-checkout-mu.vercel.app","http://localhost:5500"],
+//         methods: ["POST" , "GET" , "PATCH" , "PUT" , "DELETE" ],
+//         credentials: true
+//     }
+// ));
+app.use(cors())
+
+app.use(express.json());
+
+const apiKey = `${process.env.API_KEY}`
+const authDomain = `${process.env.AUTH_DOMAIN}`
+const databaseURL = `${process.env.DB_URL}`
+const projectId = `${process.env.PROJECT_ID}`
+const storageBucket = `${process.env.STORAGE_BUCKET}`
+const messagingSenderId = `${process.env.MESSAGING_SENDER_ID}`
+const appId = `${process.env.APP_ID}`
+const measurementId = `${process.env.MEASUREMENT_ID}`
+  
+const firebaseConfig = {
+    apiKey,
+    authDomain,
+     databaseURL,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
+    measurementId
+}
+
+
+const port = process.env.PORT || 5186;
+
+app.get('/',(req,res)=>{
+    res.send('hello guys....');
+})
+
+app.get('/firebase-config',(req,res)=>{
+    res.send(firebaseConfig);
+})
+
+app.listen(port,()=>{
+    console.log(`server is listening on port ${port}`);
+})
